b6187a4af5c3c0a93498b000bf3c2f54a6e8a4b2
d-najd <dimitar.hristijan.najdovski@gmail.com>
2026-02-21 19:17:37 +0100

    adding tests

---
+++ hibernate-core/src/test/java/org/hibernate/amine/entity/TestBaseEntity.java
@@ -0,0 +1,39 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright Red Hat Inc. and Hibernate Authors
+ */
+package org.hibernate.amine.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.NamedQuery;
+
+@Entity
+@NamedQuery(
+	name = "TestBaseEntity.findByName",
+	query = "select e from TestBaseEntity e where e.name = :name"
+)
+public class TestBaseEntity {
+
+	@Id
+	@GeneratedValue
+	public Long id;
+
+	public String name;
+
+	public TestBaseEntity() {
+	}
+
+	public TestBaseEntity(final String name) {
+		this.name = name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+}
---
+++ hibernate-core/src/test/java/org/hibernate/amine/entity/TestVersionedEntity.java
@@ -0,0 +1,49 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright Red Hat Inc. and Hibernate Authors
+ */
+package org.hibernate.amine.entity;
+
+import java.sql.Timestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.NamedQuery;
+import jakarta.persistence.Version;
+
+@Entity
+@NamedQuery(
+	name = "TestVersionedEntity.findByName",
+	query = "select e from TestVersionedEntity e where e.name = :name"
+)
+public class TestVersionedEntity {
+
+	@Id
+	@GeneratedValue
+	public Long id;
+
+	@Version
+	private Timestamp version;
+
+	public String name;
+
+	public TestVersionedEntity() {
+	}
+
+	public TestVersionedEntity(final String name) {
+		this.name = name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public Timestamp getVersion() {
+		return version;
+	}
+
+	public String getName() {
+		return name;
+	}
+}
